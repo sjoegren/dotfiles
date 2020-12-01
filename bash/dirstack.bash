489e16a8a48c189a0f260d955ddd31e91f5a391b
Aksel Sjögren <akselsjogren@users.noreply.github.com>
2020-12-01 13:06:49 +0100

    dirstack: pds function

--- bash/dirstack.bash
+++ bash/dirstack.bash
@@ -1,20 +1,30 @@
+_dirstack_file=/var/run/user/1000/dirstack.txt
+dirstack() {
+	cat <<-'EOF'
+	dirstack commands:
+	  pd   [DIR | ID]       go to new DIR, stack ID or dir on top of stack without argument.
+	  pdl  [ID]             list dirstack or copy dir of ID.
+	  pds  [REGEX]          go to first dir matching REGEX, or prompt for index.
+	EOF
+}
+
 _init_dirstack() {
-	touch /var/run/user/1000/dirstack.txt
-	_read_dirstack
+	touch $_dirstack_file
+	_read_dirstack ${1:-$_dirstack_file}
 }
 
 _read_dirstack() {
-	mapfile -t < <(sort -k 1 -t '|' -n -r /var/run/user/1000/dirstack.txt) _dirstack || return
+	mapfile -t < <(sort -k 1 -t '|' -n -r "$1") _dirstack || return
 }
 
 _update_dirstack() {
-	_init_dirstack
+	local entry dir timestamp
 	# Put current directory on top of file
-	echo "$(date +%s)|$PWD" > /var/run/user/1000/dirstack.txt
+	echo "$(date +%s)|$PWD" > $_dirstack_file
 	for entry in "${_dirstack[@]}"; do
 		IFS='|' read -r timestamp dir <<<"$entry"
 		if [ "$dir" != "$PWD" ]; then
-			echo "$entry" >> /var/run/user/1000/dirstack.txt
+			echo "$entry" >> $_dirstack_file
 		fi
 	done
 }
@@ -29,6 +39,7 @@ _dirstack_get() {
 		echo ${_dirstack[$1]}
 		return
 	fi
+	local entry dir timestamp
 	for entry in "${_dirstack[@]}"; do
 		IFS='|' read -r timestamp dir <<<"$entry"
 		if [ "$dir" != "$PWD" ]; then
@@ -42,7 +53,7 @@ _dirstack_get() {
 
 # pd [dir | index]
 pd() {
-	_init_dirstack
+	[ -z "$pds_found" ] && _init_dirstack
 	local entry timestamp dir
 	if [[ -z "$1" || $1 =~ ^[0-9]+$ ]]; then
 		entry=$(_dirstack_get ${1:-}) || return
@@ -57,16 +68,62 @@ pd() {
 	unset _dirstack
 }
 
+# pds [regex]
+pds() {
+	local dir timestamp
+	_init_dirstack
+	if [ -z "$1" ]; then
+		_print_stack
+		local sel
+		read -p "go to index (empty cancels): " sel
+		if [[ $sel =~ ^[0-9]+$ ]]; then
+			pd $sel
+		fi
+		unset sel
+		return
+	fi
+	for ((i = 0; i <= ${#_dirstack[*]} - 1; i++)); do
+		# check if pattern $1 matches dirstack entry, except in $HOME
+		if echo "${_dirstack[$i]//$HOME}" | grep -q -P "$1"; then
+			IFS='|' read -r timestamp dir <<<"${_dirstack[$i]}"
+			if [ "$dir" != "$PWD" ]; then
+				printf "Found: %s   %s\n" "$(date -d@$timestamp '+%F %T')" "$dir"
+				pds_found="$dir"
+				break
+			fi
+		fi
+	done
+	if [ -n "$pds_found" ]; then
+		pd "$pds_found"
+	else
+		echo "no match found"
+	fi
+	unset pds_found
+	return
+}
+
+_print_stack() {
+	local dir timestamp
+	for ((i = 0; i <= ${#_dirstack[*]} - 1; i++)); do
+		IFS='|' read -r timestamp dir <<<"${_dirstack[$i]}"
+		printf "%2d  %s   %s\n" $i "$(date -d@$timestamp '+%F %T')" "$dir"
+	done
+}
+
 pdl() {
-	if ! [ -f "/var/run/user/1000/dirstack.txt" ]; then
+	if ! [ -f "$_dirstack_file" ]; then
 		echo "Dirstack is empty, add with: pd DIR"
 		return
 	fi
+	local entry dir timestamp
 	_init_dirstack
-	for ((i = 0; i <= ${#_dirstack[*]} - 1; i++)); do
-		IFS='|' read -r timestamp dir <<<"${_dirstack[$i]}"
-		printf "%2d  %s   %s\n" $i "$(date -d@$timestamp +%T)" "$dir"
-	done
+	if [ -n "$1" ] && [ -n "$TMUX" ]; then
+		entry=$(_dirstack_get ${1:-}) || return
+		IFS='|' read -r _ dir <<<"$entry"
+		echo $dir | _capture_output
+	else
+		_print_stack
+	fi
 	unset _dirstack
 }
 
